cb68b0d831a83a77492635ea806e59d065fdf01b
NesrinU <ulutepenesrin@gmail.com>
2021-12-20 13:13:47 +0100

    updated

---
+++ lesson-notebook/upper_package/my_package2/my_module_4.py
@@ -0,0 +1,18 @@
+def hello():
+    return "Hello Nesrin"
+    
+
+
+def calculatr(a, opr, b):
+    if opr == "+" :
+        return a+b
+    if opr == "-" : 
+        return a-b
+    if opr == "*" : 
+        return a*b
+    if opr == "/" : 
+        return a/b
+
+
+
+person= {'name': 'nesrin', 'profession': 'DS'}
